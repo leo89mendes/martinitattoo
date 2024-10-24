652e0498acc4d312f56c94e2374121fb28638225
Leonardo Mendes <leo.89mendes@gmail.com>
2024-10-24 00:31:18 -0300

    subindo pagina de construção

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,8 +6,16 @@
         <meta name="application-name" content="{{ config('app.name') }}" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        <title>{{ config('app.name') }}</title>
+        <meta name="title" content="Martini Tattoo">
+        <meta name="keywords" content="Tatuagem, Tatuagem de animais, Tattoo, Tattoo Brooklin, Hangar Tatto">
+        <meta name="description" content="Martini Tatto, tatuando a mais de 15 anos.">
+        <meta name="author" content="Guilherme Martini">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="Martini Tattoo">
+        <meta property="og:description" content="A mais de 15 anos fazendo todos os tipos de tatuagem.">
+        <meta property="og:url" content="https://www.martinitattoo.com">
+        <meta property="og:image" content="asset(assets/img/theme.jpg)">
+        <title>Martini Tattoo</title>
 
         <style>
             [x-cloak] {
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Home::class);
