e808093c84c0bba62583a1c6c6c7661548df80be
Leonardo Mendes <leo.89mendes@gmail.com>
2024-11-07 11:55:20 -0300

    comitando page underconstruction

---
+++ resources/views/erros/503.blade.php
@@ -0,0 +1,11 @@
+<div class="container bg-cover bg-center h-screen w-full" style="background-image: url('storage/assets/img/bg.jpg')">
+    <div class="flex justify-center flex-col items-center h-full">
+        <h2 class="text-white bg-black font-bold p-4 mt-4 text-3xl text-center">Em Breve, nosso novo site, aguardem.</h2>
+        <h3 class="text-white bg-black font-bold p-4 mt-4 text-3xl text-center">Visite nosso instagram: <a class=" hover:text-[#4caf50]" href="https://instagram.com/martinitattoo" target="_blank">@martinitattoo</a></h3>
+        <h3 class="text-white font-bold p-4 mt-4 text-3xl text-center">
+            <a target="_blank" href="https://wa.me/11999722054">    
+                <img alt="Fale conosco" width="65" src="{{ asset('storage/assets/img/whatsapp.png') }}"/>
+            </a>
+        </h3>
+    </div>
+</div>
